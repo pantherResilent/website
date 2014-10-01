a7fda4e1fb091feb5e3d4132e56e73672c0fbf46
Jake Gutierrez <theborgdude@gmail.com>
2014-10-01 17:43:48 -0500

    Update Footer, 404, added logo

--- parts/footer.php
+++ parts/footer.php
@@ -1,9 +1,5 @@
 <div class="container footer">
   <hr/>
-	<div class="row">
-    <?php dynamic_sidebar('footer-widget-area'); ?>
-  </div>
-  <hr/>
   <div class="row">
     <div class="col-lg-12">
       <p>&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a></p>
